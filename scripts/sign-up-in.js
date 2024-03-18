a763343e1071f184c6ef15f8631e505cc95b7737
Will Eardley <eardley.will@gmail.com>
2024-03-18 21:07:38 +0000

    Sign-In-Up JS switch

--- scripts/sign-up-in.js
+++ scripts/sign-up-in.js
@@ -1,13 +1,30 @@
 
 // HIDE Sign-In form && SHOW Sign-Up form
+
 $(document).ready(function(){
+    //$( "#sign-up" ).hide();
+
     $("#sign-up-button").click(function(){
-        $("#sign-in").hide();
+        $("#sign-in").hide()
         $("#sign-up").show();
+        return false;
     });
 
     $("#sign-in-button").click(function(){
         $("#sign-up").hide();
         $("#sign-in").show();
+        return false;
     });
+
+    if($( '#sign-in' ).is(":visible"))
+    {
+        $( '#sign-up' ).fadeOut();
+    } 
+    else
+    {
+        $( '#sign-in' ).fadeIn();
+    }
+    
+    
 });
+
--- sign-up-in.php
+++ sign-up-in.php
@@ -78,6 +78,36 @@
         </div>
     </div>
 
+    <div id="sign-up" class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 py-5">
+            <div class="col-lg-7 text-center text-light text-lg-start">
+                <h1 class="display-4 fw-bold lh-1 mb-3">Sign up.</h1>
+                <p class="col-lg-10 fs-4">To start your journey.</p>
+            </div>
+            <div class="col-md-10 mx-auto col-lg-5">
+                <form class="p-4 p-md-5 border rounded-3 bg-light">
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email / Username</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Confirm Password</label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                <hr class="my-4">
+                <div class="text-center">
+                    <button id="sign-in-button" class="btn btn-secondary btn-md">Or sign in here.</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <!--Boostrap 5.0.2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!--jQuery 3.7.1 JS -->
