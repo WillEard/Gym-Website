4bd5ca81917a01b895570ca68700543ef70297b2
Will Eardley <eardley.will@gmail.com>
2024-03-28 18:41:24 +0000

    Error fixing / minor changes

---
+++ profile.php
@@ -0,0 +1,173 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--Custom Styling-->
+    <link rel="stylesheet" href="styles/navbar-style.css">
+</head>
+<body>
+    <!--Navbar-->
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark py-3 navbar-scrolled rounded-bottom fixed-top container">
+            <div class="container-fluid">
+                <svg width="30" height="24" class="d-inline-block align-text-top mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#FFFFFF" d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"/></svg>
+                <a class="navbar-brand fw-bold" href="#">The Gym</a> 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-light fw-bold" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="index.php#gym-groups">Gym Groups</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="index.php#pricing">Pricing</a>
+                        </li>
+                        <?php
+                          if (isset($_SESSION["username"]))
+                          {
+                            echo '<div class="btn-group">
+                            <button type="button" class="btn btn-info">'. $_SESSION["username"]. '</button>
+                            <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                              <span class="visually-hidden">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">Book a session</a></li>
+                              <li><a class="dropdown-item" href="#">Gym Plan</a></li>
+                              <li><hr class="dropdown-divider"></li>
+                              <li class="nav-item"> <a class="dropdown-item text-dark" href="form-data-handling/signout.inc.php">Sign Out</a> </li>
+                              </ul>
+                          </div>';
+                          }
+                          else
+                          {
+                            echo '<li class="nav-item"> <a class="nav-link text-light" href="sign-up-in.php">Sign In</a> </li>';
+                          }
+                        ?>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="container py-5"></div>
+        <div class="container-fluid" style="overflow-x: hidden;">
+            <div style="margin-top: 10px;" class="row extra_margin">
+                <div class="col-md-4">
+                    <div class="text-center">
+                        <h2>Profile</h2>
+                        <hr/>
+                        <img class="img-fluid" src="https://via.placeholder.com/250" alt="Profile Image"><br><br>
+
+                        <div class="file btn btn-md btn-primary col-md-6">Change Profile Picture
+							<input style="opacity: 0; position: absolute;" type="file" name="file"/> 
+						</div> 
+
+                        <hr/>
+
+                        <a class="btn btn-success btn-lg" href="Sell.php" role="button">List Vehicle For Sale</a>
+
+                        <hr/>
+                        <hr/>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h2 class="text-center">Account Information</h2>
+                    <hr/>
+                    <form>
+                        <div class="row">
+                            <div class="form-group col-md-5">
+                                <label>First Name:</label>
+                                <input type='text' class='form-control' placeholder='First Name' readonly>
+                           </div>
+                            <div class="form-group col-md-5">
+                                <label for="">Surname:</label>
+                                <input type='text' class='form-control' placeholder='Surname' readonly>
+                            </div>
+                        </div>
+                        <div class="row my-3">
+                            <div class="form-group col-md-5">
+                                <label for="">Username:</label>
+                                <input type='text' class='form-control' placeholder='Username' readonly>
+                            </div>
+                            <div class="form-group col-md-5">
+                                <label>Email Address:</label>
+                                <input type='email' class='form-control' placeholder='Email Address' readonly>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-5">
+                                <label>Change Password:</label>
+                                <button type="button" class="btn btn-md btn-warning col-md-12" data-toggle="modal" data-target="#PasswordModal">Change Password</button>
+                                <small id="passwordsmalltext" class="form-text text-muted">Change the account password</small>
+                                <div class="modal fade" id="PasswordModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Change Password</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h5 class="text-center mx-auto">Warning</h5>
+                                            <h6 class="text-center text-danger">This can only be done once every 24 Hours.</h6>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button onclick="window.location.href='php-includes/ChangePasswordProcess.php';" type="button" class="btn btn-warning">Change Password</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr/>
+                            </div>
+                        
+                            <div class="form-group col-md-5">
+                                <label for="">Delete Account:</label>
+                                <button type="button" class="btn btn-md btn-danger col-md-12" data-toggle="modal" data-target="#DeleteModal">Terminate Account</button>
+                                <small id="deleteaccountsmalltext" class="form-text text-muted">Permanently delete your account and all data</small>
+
+                                <div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Terminate Account</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <h5 class="text-center mx-auto">Are you sure you want to delete your account?</h5>
+                                            <h6 class="text-center text-danger">All data will be permanently lost.</h6>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button onclick="window.location.href='php-includes/DeleteProcessLoading.php';" type="button" class="btn btn-danger">Delete Account</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>  
+                                <hr/>
+                            </div>
+                        </div>
+                    </form>                    
+                </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+    
